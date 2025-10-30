670c576b7ede9af6faac50cb9d5729d80b882ad6
6cv2 <peterandrews@rhs.school.nz>
2025-10-31 11:29:42 +1300

    Create sketch_oct31a.ino

---
+++ sketch_oct31a/sketch_oct31a.ino
@@ -0,0 +1,25 @@
+
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 9;  // Analog output pin that the LED is attached to
+
+int sensorValue = 0;  // value read from the pot
+int outputValue = 0;  // value output to the PWM (analog out)
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long rollingAverage = 0;
+
+  for(int i=0; i<500; i++){
+    sensorValue = analogRead(analogInPin);
+    rollingAverage += sensorValue;
+    delay(10);
+  }
+  float outputValue = rollingAverage/500;
+
+  Serial.print("sensor = ");
+  Serial.println(outputValue);
+}
