8527f907ac50ba5e906724bda67719aff9adf192
6cv2 <peterandrews@rhs.school.nz>
2025-10-31 13:08:47 +1300

    Led sound measurement
    Got led working

--- sketch_oct31a/sketch_oct31a.ino
+++ sketch_oct31a/sketch_oct31a.ino
@@ -1,6 +1,7 @@
 
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int analogOutPin = 9;  // Analog output pin that the LED is attached to
+const int analogOutPin = 9;
+const int ledPin = 2;  // Analog output pin that the LED is attached to
 
 int sensorValue = 0;  // value read from the pot
 int outputValue = 0;  // value output to the PWM (analog out)
@@ -8,9 +9,11 @@ int outputValue = 0;  // value output to the PWM (analog out)
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(ledPin, HIGH);
   unsigned long rollingAverage = 0;
 
   for(int i=0; i<500; i++){
