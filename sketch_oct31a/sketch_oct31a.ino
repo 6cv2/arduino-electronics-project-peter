3c8bd44b4930389061c48e8068ac42f7dd8f36a1
6cv2 <peterandrews@rhs.school.nz>
2025-11-05 14:39:53 +1300

    Final final final version
    Hopefully the last

--- sketch_oct31a/sketch_oct31a.ino
+++ sketch_oct31a/sketch_oct31a.ino
@@ -1,14 +1,14 @@
-#include <LiquidCrystal_I2C.h>    // Include the Liquid crystal library for the LCD display
+#include <LiquidCrystal_I2C.h>  // Include the Liquid crystal library for the LCD display
 
-const int ANALOG_IN_PIN = A0;     // input from sound sensor
+const int ANALOG_IN_PIN = A0;  // input from sound sensor
 
-const int LED_PIN_1 = 11;         // setting up all led pin locations
+const int LED_PIN_1 = 11;  // setting up all led pin locations
 const int LED_PIN_2 = 10;
 const int LED_PIN_3 = 9;
 const int LED_PIN_4 = 8;
 const int LED_PIN_5 = 7;
 
-const int THRESHOLD_1 = 400;      // setting up all volume level thresholds
+const int THRESHOLD_1 = 400;  // setting up all volume level thresholds
 const int THRESHOLD_2 = 450;
 const int THRESHOLD_3 = 510;
 const int THRESHOLD_4 = 550;
@@ -20,22 +20,22 @@ const int INTERRUPT_PIN = 2;       // interrupt pin location
 int sensorValue = 0;
 int averageSensorValue = 0;
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);// configures the displays text dimensions
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // configures the displays text dimensions
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_PIN_1, OUTPUT);      // LED pins
+  pinMode(LED_PIN_1, OUTPUT);  // LED pins
   pinMode(LED_PIN_2, OUTPUT);
   pinMode(LED_PIN_3, OUTPUT);
   pinMode(LED_PIN_4, OUTPUT);
   pinMode(LED_PIN_5, OUTPUT);
 
-  pinMode(INTERRUPT_PIN, INPUT);   // sets up the interrupts pin mode
-  lcd.init();                      // initializes LCD display
+  pinMode(INTERRUPT_PIN, INPUT);  // sets up the interrupts pin mode
+  lcd.init();                     // initializes LCD display
   lcd.backlight();
 
   // Attaches interrupt, defines pin, and triggers tooLoud interrupt on rising signal of INTERRUPT_PIN
-  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), tooLoud, RISING); 
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), tooLoud, RISING);
 }
 
 void loop() {
@@ -91,10 +91,11 @@ void loop() {
   if (isThresholdExceeded) {
     lcd.print("Yes");
   } else {
-      lcd.print("no");
-}}
+    lcd.print("no");
+  }
+}
 
-  // Interrupt activated if volume threshold reached
+// Interrupt activated if volume threshold reached
 void tooLoud() {
   // sets the variable to true changing what's displayed on the second line of the LCD display
   isThresholdExceeded = true;
