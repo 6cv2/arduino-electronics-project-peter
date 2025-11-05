b109b0165e836f8e15154778e9d383e354364cab
6cv2 <peterandrews@rhs.school.nz>
2025-11-05 13:13:33 +1300

    near final/final ver

--- sketch_oct31a/sketch_oct31a.ino
+++ sketch_oct31a/sketch_oct31a.ino
@@ -1,28 +1,65 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogInPin = A0;
 const int analogOutPin = 9;
-const int ledPin = 2;  // Analog output pin that the LED is attached to
+const int ledPin1 = 11;
+const int ledPin2 = 10;
+const int ledPin3 = 9;
+const int ledPin4 = 8;
+const int ledPin5 = 7;
 
-int sensorValue = 0;  // value read from the pot
-int outputValue = 0;  // value output to the PWM (analog out)
+int sensorValue = 0;
+int outputValue = 0;
+
+LiquidCrystal_I2C lcd(0x27,16,2);
 
 void setup() {
-  // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
+  lcd.init();
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
+
   unsigned long rollingAverage = 0;
 
   for(int i=0; i<500; i++){
     sensorValue = analogRead(analogInPin);
     rollingAverage += sensorValue;
-    delay(10);
-  }
-  float outputValue = rollingAverage/500;
 
+    outputValue = rollingAverage/500;}
+
+    if (outputValue > 410) {
+      digitalWrite(ledPin1, HIGH);
+    } else {
+      digitalWrite(ledPin1, LOW);}
+    if (outputValue > 425) {
+      digitalWrite(ledPin2, HIGH);
+    } else {
+      digitalWrite(ledPin2, LOW);}
+    if (outputValue > 460) {
+      digitalWrite(ledPin3, HIGH);
+    } else {
+      digitalWrite(ledPin3, LOW);}
+    if (outputValue > 500) {
+      digitalWrite(ledPin4, HIGH);
+    } else {
+      digitalWrite(ledPin4, LOW);}
+    if (outputValue > 600) {
+      digitalWrite(ledPin5, HIGH);
+    } else {
+      digitalWrite(ledPin5, LOW);}
+  
   Serial.print("sensor = ");
   Serial.println(outputValue);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Value: ");
+  lcd.print(outputValue);
+  lcd.print("   ");
 }
